c6597670251785f67af5888dd42df9f27c5361c0
Stephen Johnson <stephenjayyy@gmail.com>
2021-08-24 15:02:46 -0500

    Add while condition to exit/continue app and changed numbers to decimal due to zero divide error

--- Program.cs
+++ Program.cs
@@ -6,64 +6,93 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Console.WriteLine("Welcome to Stephen's Calculator v.1\n\n\n\n\n");
-
-                //Reuqesting first number from user
-                Console.Write("Hello, enter the first number:  ");
-                double firstNumber = Convert.ToDouble(Console.ReadLine());
-                Console.Write("\n\n\n");
-
-                //Requesting second number from user
-                Console.Write("Enter the second number:  ");
-                double secondNumber = Convert.ToDouble(Console.ReadLine());
-                Console.Write("\n\n\n");
-
-                //Requesting operator from user
-                Console.WriteLine("Please select an operator: + | - | * | /  ");
-                Console.Write("\n");
-                Console.Write("Selection:  ");
-                string operation = Console.ReadLine();
-                Console.Write("\n\n\n");
-
-                
-
-                //Checks if user imputted one of the follwowing: - , + , * , /
-                if (operation == "-")
+            while (true)
+                try
                 {
-                    Console.WriteLine($"Result: {firstNumber} - {secondNumber} = " + (firstNumber - secondNumber));
-                }
-                else if (operation == "+")
-                {
-                    Console.WriteLine($"Result: {firstNumber} + {secondNumber} = " + (firstNumber + secondNumber));
-                }
-                else if (operation == "*")
-                {
-                    Console.WriteLine($"Result: {firstNumber} * {secondNumber} = " + (firstNumber * secondNumber));
-                }
-                else if (operation == "/")
-                {
-                    Console.WriteLine($"Result: {firstNumber} / {secondNumber} = " + (firstNumber / secondNumber));
-                }
-                //If user inputs anything other than available operators, error is called from WrongOperation method
-                else
-                {
-                    //Method is invoked to print operators
-                    OperationList();
-                }
-            }
+                    Console.WriteLine("Welcome to Stephen's Calculator v.1\n\n\n\n\n");
 
-            //If user inputs anything but a number, displays error message
-            catch (FormatException)
-            {
-                Console.WriteLine("please use numbers only");
-            }
-            //If user attempts to divide by zero, displays error message
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Please do not divide my zero.");
-            }
+                    //Reuqesting first number from user
+                    Console.Write("Hello, enter the first number:  ");
+                    decimal firstNumber = Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("\n\n\n");
+
+                    //Requesting second number from user
+                    Console.Write("Enter the second number:  ");
+                    decimal secondNumber = Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("\n\n\n");
+
+                    //Requesting operator from user
+                    Console.WriteLine("Please select an operator: + | - | * | /  ");
+                    Console.Write("\n");
+                    Console.Write("Selection:  ");
+                    string operation = Console.ReadLine();
+                    Console.Write("\n\n\n");
+
+
+
+                    //Checks if user imputted one of the follwowing: - , + , * , /
+                    if (operation == "-")
+                    {
+                        Console.WriteLine($"Result: {firstNumber} - {secondNumber} = " + (firstNumber - secondNumber));
+                    }
+                    else if (operation == "+")
+                    {
+                        Console.WriteLine($"Result: {firstNumber} + {secondNumber} = " + (firstNumber + secondNumber));
+                    }
+                    else if (operation == "*")
+                    {
+                        Console.WriteLine($"Result: {firstNumber} * {secondNumber} = " + (firstNumber * secondNumber));
+                    }
+                    else if (operation == "/")
+                    {
+                        Console.WriteLine($"Result: {firstNumber} / {secondNumber} = " + (firstNumber / secondNumber));
+                    }
+
+                    //If user inputs anything other than available operators, error is called from WrongOperation method
+                    else
+                    {
+                        //Method is invoked to print operators
+                        OperationList();
+                    }
+                    Console.WriteLine("\n\n Do you want to exit this program? (y/n)");
+                    var status = Console.ReadKey();
+                    if (status.Key == ConsoleKey.Y)
+                    {
+                        break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+
+
+                //If user inputs anything but a number, displays error message
+                catch (FormatException)
+                {
+                    Console.WriteLine("please use numbers only");
+                    Console.WriteLine("\n\n Do you want to exit this program? (y/n)");
+                    var status = Console.ReadKey();
+                    if (status.Key == ConsoleKey.Y)
+                    {
+                        break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+                //If user attempts to divide by zero, displays error message
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Please do not divide my zero.");
+                    Console.WriteLine("\n\n Do you want to exit this program? (y/n)");
+                    var status = Console.ReadKey();
+                    if (status.Key == ConsoleKey.Y)
+                    {
+                        break;
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
 
             //Created method to print out correct operators if user inputted wrong value
             static void OperationList()
@@ -74,9 +103,8 @@ namespace Calculator
                 Console.WriteLine("/ for division");
                 Console.WriteLine("* for multiplication");
             }
-
-            
             Console.ReadKey();
         }
     }
 }
+
